8aa300f09efdc1094049ec75cb80c0cddb8736d8
Toby Courtis <toby.courtis@hotmail.com>
2022-05-28 17:00:44 +0100

    [TC/JF] Moved to a working game of breakout using openai gym

---
+++ breakout.py
@@ -0,0 +1,11 @@
+import gym
+env = gym.make("ALE/Breakout-v5", render_mode="human")
+observation, info = env.reset(seed=42, return_info=True)
+
+for _ in range(10000):
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+
+    if done:
+        observation, info = env.reset(return_info=True)
+env.close()
